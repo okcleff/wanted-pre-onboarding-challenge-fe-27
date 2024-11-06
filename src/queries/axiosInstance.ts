4a243a9d204b402d258186291981da41fbf6f2c5
okcleff <okcleff@gmail.com>
2024-11-06 14:16:39 +0900

    refactor: api url env 처리

--- src/queries/axiosInstance.ts
+++ src/queries/axiosInstance.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 import { AuthInstance } from "../utils/auth";
 
-const BASE_URL = "http://localhost:8080";
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8080";
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
